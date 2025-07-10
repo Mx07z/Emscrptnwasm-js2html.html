4406e5c2902f825ba4af4643488f8d79fb42d501
Mx07z <znesln7@hotmail.com>
2025-07-10 09:28:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Serve your HTML file at the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Emscrptnwasm+js2html.html'));
+});
+
+// Serve all other static files in the directory (if any)
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(__dirname + '/Emscrptnwasm+js2html.html'));
+
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
